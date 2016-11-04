1c4816e8792b6a6590deb50c715114edbd99cd33
Mikunj varsani <mikunj@live.com.au>
2016-11-04 16:51:50 +1100

    Initial commit

---
+++ Ookami/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ookami
+//
+//  Created by Maka on 4/11/16.
+//  Copyright © 2016 Mikunj Varsani. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds);
+        window?.rootViewController = ViewController();
+        window?.makeKeyAndVisible();
+        
+        return true
+    }
+
+}
+
