7cc7c56bb8ee536d18fdbbc43182ad2264c27ef5
Mikunj varsani <mikunj@live.com.au>
2016-11-04 17:04:49 +1100

    Added OokamiKit Framework

---
+++ OokamiKit/OokamiKit.h
@@ -0,0 +1,19 @@
+//
+//  OokamiKit.h
+//  OokamiKit
+//
+//  Created by Maka on 4/11/16.
+//  Copyright © 2016 Mikunj Varsani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OokamiKit.
+FOUNDATION_EXPORT double OokamiKitVersionNumber;
+
+//! Project version string for OokamiKit.
+FOUNDATION_EXPORT const unsigned char OokamiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OokamiKit/PublicHeader.h>
+
+
