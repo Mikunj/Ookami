3995431b4fd67f5d18e8647c2ce58375a6469d80
Mikunj varsani <mikunj@live.com.au>
2017-01-14 13:31:57 +1100

    Refactored MediaViewController and split functionality into NavigationHidingViewController.

---
+++ Ookami/Shared/Controllers/NavigationHidingViewController.swift
@@ -0,0 +1,138 @@
+//
+//  NavigationHidingViewController.swift
+//  Ookami
+//
+//  Created by Maka on 14/1/17.
+//  Copyright © 2017 Mikunj Varsani. All rights reserved.
+//
+
+import UIKit
+import Cartography
+
+//A controller which has a custom navigation bar which is transparent but after a certain offset it becomes visible
+class NavigationHidingViewController: UIViewController {
+    
+    //The top view to emulate a navigation bar set by the system
+    lazy var barView: UIView = {
+        let v = UIView()
+        v.backgroundColor = Theme.NavigationTheme().barColor
+        return v
+    }()
+    
+    //The height constraint of the top view
+    var barViewHeight: NSLayoutConstraint?
+    
+    //The navigation bar we use so we can get transparancy
+    lazy var navigationBar: UINavigationBar = {
+        let bar = UINavigationBar()
+        let theme = Theme.NavigationTheme()
+        bar.isTranslucent = true
+        bar.barTintColor = UIColor.clear
+        bar.tintColor = theme.barTextColor
+        bar.titleTextAttributes = [NSForegroundColorAttributeName: theme.barTextColor]
+        bar.setBackgroundImage(UIImage(), for: .default)
+        bar.shadowImage = UIImage()
+        
+        let item = UINavigationItem(title: "")
+        
+        let close = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.stop, target: self, action: #selector(didDismiss))
+        item.leftBarButtonItem = close
+        
+        bar.setItems([item], animated: false)
+        
+        return bar
+    }()
+    
+    //Variable to check if current VC is modal
+    private var isModal: Bool {
+        return self.presentingViewController?.presentedViewController == self
+            || (self.navigationController != nil && self.navigationController?.presentingViewController?.presentedViewController == self.navigationController)
+            || self.tabBarController?.presentingViewController is UITabBarController
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.automaticallyAdjustsScrollViewInsets = false
+        barView.isHidden = false
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+        updateNavigationBar()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Add the bar
+        barView.addSubview(navigationBar)
+        constrain(navigationBar) { bar in
+            bar.bottom == bar.superview!.bottom
+            bar.left == bar.superview!.left
+            bar.right == bar.superview!.right
+            bar.height == 44
+        }
+        
+        //The top view
+        self.view.addSubview(barView)
+        constrain(barView) { view in
+            view.top == view.superview!.top
+            view.left == view.superview!.left
+            view.right == view.superview!.right
+            barViewHeight = (view.height == 44)
+        }
+        updateTopViewHeight()
+        
+        //May need to call this again in subclass
+        self.view.bringSubview(toFront: barView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateTopViewHeight()
+    }
+    
+    //Update the height of the top view incase of orientation changes
+    func updateTopViewHeight() {
+        let statusHeight = UIApplication.shared.statusBarFrame.height
+        let barHeight = self.navigationController?.navigationBar.frame.size.height ?? 44
+        barViewHeight?.constant = barHeight + statusHeight
+        barView.layoutIfNeeded()
+    }
+    
+    //The offset that makes the bar visible
+    func visibleOffset() -> CGFloat {
+        return 0
+    }
+    
+    //The title to put on the bar
+    func barTitle() -> String {
+        return ""
+    }
+    
+    /// The function to update the custom navigation bar display
+    /// Call this in scrollViewDidScroll(:)
+    func updateNavigationBar(offset: CGFloat = 0) {
+        let minOffset: CGFloat = visibleOffset()
+        
+        UIView.animate(withDuration: 0.3) {
+            let shouldShow = offset > minOffset
+            let color = shouldShow ? Theme.NavigationTheme().barColor : UIColor.clear
+            self.navigationBar.topItem?.title = shouldShow ? self.barTitle() : ""
+            self.barView.backgroundColor = color
+        }
+    }
+    
+    //The dismiss button was pressed
+    //We handle both cases incase we forget to present modally
+    func didDismiss() {
+        if isModal {
+            self.dismiss(animated: true)
+        } else {
+            let _ = self.navigationController?.popViewController(animated: true)
+        }
+    }
+
+}
--- Ookami/Shared/Media/MediaViewController.swift
+++ Ookami/Shared/Media/MediaViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 import Cartography
 
+//A section of data that can be displayed in the controller
 struct MediaViewControllerSection {
     var title: String
     var cellCount: Int
     var cellForIndexPath: (IndexPath, UITableView) -> UITableViewCell = { _,_ in return UITableViewCell() }
     var willDisplayCell: ((IndexPath, UITableViewCell) -> Void)? = nil
     var didSelectRow: ((IndexPath, UITableView) -> Void)? = nil
+    var heightForRow: (IndexPath) -> CGFloat = { _ in
+        return UITableViewAutomaticDimension
+    }
     
     init(title: String, cellCount: Int) {
         self.title = title
@@ -22,40 +26,8 @@ struct MediaViewControllerSection {
     }
 }
 
-//TODO: We can extract this into NavigationHidingViewController which can also be used by UserViewController.
-
-class MediaViewController: UIViewController {
-    
-    //The top view to emulate a navigation bar set by the system
-    lazy var topView: UIView = {
-        let v = UIView()
-        v.backgroundColor = Theme.NavigationTheme().barColor
-        return v
-    }()
-    
-    //The height constraint of the top view
-    var topViewHeight: NSLayoutConstraint?
-    
-    //The navigation bar we use so we can get transparancy
-    lazy var navigationBar: UINavigationBar = {
-        let bar = UINavigationBar()
-        let theme = Theme.NavigationTheme()
-        bar.isTranslucent = true
-        bar.barTintColor = UIColor.clear
-        bar.tintColor = theme.barTextColor
-        bar.titleTextAttributes = [NSForegroundColorAttributeName: theme.barTextColor]
-        bar.setBackgroundImage(UIImage(), for: .default)
-        bar.shadowImage = UIImage()
-        
-        let item = UINavigationItem(title: "")
-        
-        let close = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.stop, target: self, action: #selector(didDismiss))
-        item.leftBarButtonItem = close
-        
-        bar.setItems([item], animated: false)
-        
-        return bar
-    }()
+//A controller which allows the display of media
+class MediaViewController: NavigationHidingViewController {
     
     //The tableview to display data in
     lazy var tableView: UITableView = {
@@ -83,6 +55,7 @@ class MediaViewController: UIViewController {
         return t
     }()
     
+    //The height of the header
     var headerHeight: CGFloat {
         let isIpad = UIDevice.current.userInterfaceIdiom == .pad
         return isIpad ? 464 : 344
@@ -95,6 +68,7 @@ class MediaViewController: UIViewController {
         return MediaTableHeaderView(frame: CGRect(origin: .zero, size: CGSize(width: width, height: height)))
     }()
     
+    //The layout constraints used for parallax
     var mediaHeaderTop: NSLayoutConstraint?
     var mediaHeaderHeight: NSLayoutConstraint?
     
@@ -104,19 +78,12 @@ class MediaViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.automaticallyAdjustsScrollViewInsets = false
-        
-        topView.isHidden = false
-        navigationController?.setNavigationBarHidden(true, animated: animated)
-        
         //Force the scrollview scroll
         scrollViewDidScroll(tableView)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        topView.isHidden = true
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
@@ -139,47 +106,12 @@ class MediaViewController: UIViewController {
             mediaHeaderHeight = (header.height == headerHeight)
         }
         
-        //Add the bar
-        topView.addSubview(navigationBar)
-        constrain(navigationBar) { bar in
-            bar.bottom == bar.superview!.bottom
-            bar.left == bar.superview!.left
-            bar.right == bar.superview!.right
-            bar.height == 44
-        }
-        
-        //The top view
-        self.view.addSubview(topView)
-        constrain(topView) { view in
-            view.top == view.superview!.top
-            view.left == view.superview!.left
-            view.right == view.superview!.right
-            topViewHeight = (view.height == 44)
-        }
-        updateTopViewHeight()
-        
-        self.view.bringSubview(toFront: topView)
-        
         //Force tableview layout
         tableView.setNeedsLayout()
         tableView.layoutIfNeeded()
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        updateTopViewHeight()
-    }
-    
-    //Update the height of the top view incase of orientation changes
-    func updateTopViewHeight() {
-        let statusHeight = UIApplication.shared.statusBarFrame.height
-        let barHeight = self.navigationController?.navigationBar.frame.size.height ?? 44
-        topViewHeight?.constant = barHeight + statusHeight
-        topView.layoutIfNeeded()
-    }
-    
-    func didDismiss() {
-        self.dismiss(animated: true)
+        
+        //Bring the bar at the front
+        self.view.bringSubview(toFront: barView)
     }
     
     func reloadData() {
@@ -198,8 +130,12 @@ class MediaViewController: UIViewController {
     }
     
     //The title to show on the navigation bar
-    func barTitle() -> String {
+    override func barTitle() -> String {
         return "Media"
+    }
+    
+    override func visibleOffset() -> CGFloat {
+        return -100
     }
     
 }
@@ -221,6 +157,10 @@ extension MediaViewController: UITableViewDataSource {
         let cell = section.cellForIndexPath(indexPath, tableView)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return data[indexPath.section].heightForRow(indexPath)
     }
 }
 
@@ -252,19 +192,6 @@ extension MediaViewController: UITableViewDelegate {
 //MARK:- Scroll view delegate
 extension MediaViewController {
     
-    //Update the display of the navigation bar
-    func updateNavigationBar(offset: CGFloat) {
-        
-        let minOffset: CGFloat = 100
-        
-        UIView.animate(withDuration: 0.3) {
-            let shouldShow = offset > -minOffset
-            let color = shouldShow ? Theme.NavigationTheme().barColor : UIColor.clear
-            self.navigationBar.topItem?.title = shouldShow ? self.barTitle() : ""
-            self.topView.backgroundColor = color
-        }
-    }
-    
     func updateHeaderConstraints(offset: CGFloat) {
         //We use the diff because media header is attached to the top of the base view
         //We need to know how much we moved up / down
@@ -282,8 +209,8 @@ extension MediaViewController {
         mediaHeaderHeight?.constant = max(headerHeight - excessHeight, -offset)
         
         //This will cause a space to be left at the bottom if the table contents height is less than the header height.
-        let topViewHeight = topView.frame.height
-        let topInset = min(headerHeight, max(topViewHeight, -offset))
+        let barViewHeight = barView.frame.height
+        let topInset = min(headerHeight, max(barViewHeight, -offset))
         tableView.contentInset = UIEdgeInsets(top: topInset, left: 0, bottom: 0, right: 0)
     }
     
