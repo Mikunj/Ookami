4a9f177d83b5a7d57158ea8313c643edbe17cf2c
Mikunj varsani <mikunj@live.com.au>
2017-05-07 11:37:14 +1000

    Fixed rounded rating display.

--- Ookami/Extenstions/Double+Ookami.swift
+++ Ookami/Extenstions/Double+Ookami.swift
@@ -12,6 +12,6 @@ extension Double {
     func round(nearest: Double) -> Double {
         let n = 1/nearest
         let numberToRound = self * n
-        return numberToRound.rounded() / n
+        return numberToRound.rounded(.down) / n
     }
 }
